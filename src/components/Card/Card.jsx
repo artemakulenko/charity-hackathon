fe74a15d2846edcd7190245a4fcb7da26fe4961b
irenekurbanova <kirina2504@gmail.com>
2023-11-11 16:59:07 +0300

    cards logic, in progress

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,17 @@
 import styles from "./Card.module.css";
 
-export const Card = () => {
-	return <h1 className={styles.title}>card</h1>;
+import jsBadge from "../../assets/images/jsbadge.svg";
+
+export const Card = ({ card, onClick }) => {
+  return (
+    <div
+      className={`memory-card${card.isFlipped ? " flip" : ""}`}
+      onClick={onClick}
+      style={{ order: card.order }}
+      data-testid={card.id}
+    >
+      <img className="front-face" src={card.image} alt="Card" />
+      <img className="back-face" src={jsBadge} alt="JS Badge" />
+    </div>
+  );
 };
--- src/components/pages/Game.jsx
+++ src/components/pages/Game.jsx
@@ -1,11 +1,75 @@
 import { useParams } from "react-router-dom";
 
+import React, { useState } from "react";
+import { Card } from "../Card/Card";
+import { cardsData } from "../../cards";
+
 export const Game = () => {
-  const { difficulty } = useParams();
+  // const { difficulty } = useParams();
+  // <div>Difficulty is {difficulty} </div>
+  let [cardsState, setCardsState] = useState(cardsData);
+
+  // kep first card info
+  let [firstCard, setFirstCard] = useState(null);
+  // is it first click?
+  let [secondClick, setSecondClick] = useState(false);
+  // set flag to wait for 1500ms
+  let [wait, setWait] = useState(false);
+
+  // functions
+  const checker = async (card) => {
+    if (card.name === firstCard.name) {
+      console.log("hellooo");
+      card["passed"] = true;
+      firstCard["passed"] = true;
+      changeCardStatusHandler(card);
+      changeCardStatusHandler(firstCard);
+    } else {
+      setWait(true);
+      setTimeout(() => {
+        changeCardStatusHandler(card);
+        changeCardStatusHandler(firstCard);
+        setWait(false);
+      }, 1500);
+    }
+  };
+
+  const changeCardStatusHandler = async (clickedCard) => {
+    if (!clickedCard.passed) clickedCard.isFlipped = !clickedCard.isFlipped;
+    let index = cardsState.findIndex((card) => card.id === clickedCard.id);
+    let newState = [...cardsState];
+    newState.splice(index, 1, clickedCard);
+    await setCardsState(newState);
+  };
+
+  const handleClick = async (e, clickedCard) => {
+    if (wait) {
+      return;
+    }
+    if (!secondClick) {
+      await setFirstCard(clickedCard);
+      await setSecondClick(true);
+      changeCardStatusHandler(clickedCard);
+    } else {
+      await setSecondClick(false);
+      changeCardStatusHandler(clickedCard);
+      checker(clickedCard);
+      setFirstCard(null);
+    }
+  };
 
   return (
-    <div>
-      <div>Difficulty is {difficulty} </div>
-    </div>
+    <section className="memory-game">
+      {cardsState?.map((card) => {
+        return (
+          <Card
+            key={card.id}
+            card={card}
+            onClick={(e) => handleClick(e, card)}
+          />
+        );
+      })}
+      {/* <Card card={card} onClick={} /> */}
+    </section>
   );
 };
