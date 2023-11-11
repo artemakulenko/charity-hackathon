aeac1b31e4d653b4ac94a604418a6aaa1d66ff4c
irenekurbanova <kirina2504@gmail.com>
2023-11-11 14:47:55 +0300

    routes, in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import "./index.css";
 import Header from "./components/Header/Header";
 import Container from "./components/UI/Container/Container";
+import { Route, Routes } from "react-router-dom";
+
+import { Game } from "./components/pages/Game";
+import { Home } from "./components/pages/Home";
 
 function App() {
-	return (
-		<>
-			<Container>
-				<Header />
-			</Container>
-		</>
-	);
+  return (
+    <>
+      <Container>
+        <Header />
+      </Container>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game/:difficulty" element={<Game />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,1 +1,18 @@
-export const Home = () => {};
+import { useNavigate } from "react-router-dom";
+
+export const Home = () => {
+  const navigate = useNavigate();
+
+  const sendSubmit = (link) => {
+    navigate(link);
+  };
+
+  return (
+    <>
+      <h1>this is home page</h1>
+      <button onClick={() => sendSubmit(`/game/easy`)}>Easy</button>
+      <button onClick={() => sendSubmit(`/game/medium`)}>Medium</button>
+      <button onClick={() => sendSubmit(`/game/hard`)}>Hard</button>
+    </>
+  );
+};
