42a37d1fb2a291225631d6c7754eafe0a55adcca
heywhyyou <havenoreasontolive@gmail.com>
2023-11-11 14:49:47 +0300

    feat: add notfound component barebones

---
+++ src/components/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Упс! Такой страницы не существует.</p>
+    </div>
+  );
+};
+
+export default NotFound;
