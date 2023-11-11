9fd654403c9ba3887d60d77b4ddde35a8f9b12e3
irenekurbanova <kirina2504@gmail.com>
2023-11-11 19:34:00 +0300

    feat: homepage in progress

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import "./index.css";
 import Header from "./components/Header/Header";
 import { Route, Routes } from "react-router-dom";
 
-import { Game } from "./components/pages/Game";
+// import { Game } from "./components/pages/Game";
 import { Home } from "./components/pages/Home";
 import NotFound from "./components/pages/NotFound";
 
 function App() {
-	return (
-		<>
-			<Header />
+  return (
+    <>
+      <Header />
 
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/game/:difficulty" element={<Game />} />
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-		</>
-	);
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game/:difficulty" element={<Game />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,18 +1,64 @@
+import Container from "../UI/Container/Container";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import styles from "./Home.module.css";
 
 export const Home = () => {
   const navigate = useNavigate();
+  const [numOfDivs, setNumOfDivs] = useState([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+  ]);
+
+  const numDivsHandler = (event) => {
+    const array = [];
+    let quantity = event.target.value;
+    for (let i = 0; i < quantity; i++) {
+      array.push(i);
+    }
+
+    setNumOfDivs(array);
+  };
 
   const sendSubmit = (link) => {
     navigate(link);
   };
 
   return (
-    <>
-      <h1>this is home page</h1>
-      <button onClick={() => sendSubmit(`/game/easy`)}>Easy</button>
-      <button onClick={() => sendSubmit(`/game/medium`)}>Medium</button>
-      <button onClick={() => sendSubmit(`/game/hard`)}>Hard</button>
-    </>
+    <Container>
+      <h1>Выберете сложность</h1>
+      <div className={styles.wrapper}>
+        <div className={styles.actions}>
+          <button
+            value={12}
+            // onClick={() => sendSubmit(`/game/easy`)}
+            onClick={(event) => numDivsHandler(event)}
+          >
+            Easy
+          </button>
+          <button
+            value={16}
+            // onClick={() => sendSubmit(`/game/medium`)}
+            onClick={(event) => numDivsHandler(event)}
+          >
+            Medium
+          </button>
+          <button
+            value={20}
+            // onClick={() => sendSubmit(`/game/hard`)}
+            onClick={(event) => numDivsHandler(event)}
+          >
+            Hard
+          </button>
+          <button type="button" onClick={() => sendSubmit(`/game/hard`)}>
+            Играть
+          </button>
+        </div>
+        <div className={styles.grid}>
+          {numOfDivs.map((el) => (
+            <div key={el}></div>
+          ))}
+        </div>
+      </div>
+    </Container>
   );
 };
