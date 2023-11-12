d3df53ce897cb33b33fa303a3e335d279609d6c0
artemakulenko <akulenkoar@yandex.ru>
2023-11-12 17:04:24 +0300

    fix: home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,18 @@
 import Logo from "../Logo/Logo";
-import Login from "../Login/Login";
+// import Login from "../Login/Login";
 import Container from "../UI/Container/Container";
 import styles from "./Header.module.css";
 
 function Header() {
-  return (
-    <header className={styles.header}>
-      <Container>
-        <div className={styles.wrapper}>
-          <Logo />
-          <Login />
-        </div>
-      </Container>
-    </header>
-  );
+	return (
+		<header className={styles.header}>
+			<Container>
+				<div className={styles.wrapper}>
+					<Logo />
+				</div>
+			</Container>
+		</header>
+	);
 }
 
 export default Header;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,72 +5,67 @@ import { Button } from "../UI/Button/Button";
 import styles from "./Home.module.css";
 
 export const Home = () => {
-  const navigate = useNavigate();
-  const [numOfDivs, setNumOfDivs] = useState([
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-  ]);
+	const navigate = useNavigate();
+	const [numOfDivs, setNumOfDivs] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+	const [description, setDescription] = useState(0);
 
-  const numDivsHandler = (event) => {
-    const array = [];
-    let quantity = event.currentTarget.value;
-    for (let i = 0; i < quantity; i++) {
-      array.push(i);
-    }
+	const numDivsHandler = event => {
+		const array = [];
+		let quantity = event.currentTarget.value;
+		for (let i = 0; i < quantity; i++) {
+			array.push(i);
+		}
 
-    setNumOfDivs(array);
-  };
+		setDescription(Number(event.currentTarget.value));
+		setNumOfDivs(array);
+	};
 
-  const sendSubmit = (link) => {
-    navigate(link);
-  };
+	const sendSubmit = link => {
+		navigate(link);
+	};
 
-  return (
-    <Container>
-      <h1>Выберете сложность</h1>
-      <div className={styles.wrapper}>
-        <div className={styles.actions}>
-          <Button
-            value={12}
-            style="primary"
-            onClick={(event) => numDivsHandler(event)}
-          >
-            Easy
-          </Button>
-          <Button
-            value={16}
-            style="primary"
-            onClick={(event) => numDivsHandler(event)}
-          >
-            Medium
-          </Button>
-          <Button
-            value={20}
-            style="primary"
-            onClick={(event) => numDivsHandler(event)}
-          >
-            Hard
-          </Button>
-          <Button
-            type="button"
-            style="action"
-            onClick={() => sendSubmit(`/game/${numOfDivs.length}`)}
-          >
-            Играть
-          </Button>
-        </div>
+	return (
+		<Container>
+			<div className={styles.wrapper}>
+				<div className={styles.actions}>
+					<h1 className={styles.title}>
+						Добро пожаловать в <br /> Homebound <span style={{ color: "red" }}>Hearts!</span>
+					</h1>
+					<h2 className={styles.h2}>
+						Выберите сложность:
+						{description ? (
+							<span>
+								{description === 12 ? " Легко, Сетка 3*4" : ""}
+								{description === 16 ? " Средне, Сетка 4*4" : ""}
+								{description === 20 ? " Сложно, Сетка 4*5" : ""}
+							</span>
+						) : (
+							""
+						)}
+					</h2>
 
-        <div
-          className={`
-            ${styles.grid} 
-            ${numOfDivs.length === 12 && styles.easy} 
-            ${numOfDivs.length === 16 && styles.medium} 
-            ${numOfDivs.length === 20 && styles.hard}`}
-        >
-          {numOfDivs.map((el) => (
-            <div key={el}></div>
-          ))}
-        </div>
-      </div>
-    </Container>
-  );
+					<div className={styles.btnGrid}>
+						<Button value={12} style="primary" onClick={event => numDivsHandler(event)}>
+							Легко
+						</Button>
+						<Button value={16} style="primary" onClick={event => numDivsHandler(event)}>
+							Средне
+						</Button>
+						<Button value={20} style="primary" onClick={event => numDivsHandler(event)}>
+							Сложно
+						</Button>
+						<Button type="button" style="action" onClick={() => sendSubmit(`/game/${numOfDivs.length}`)}>
+							Играть
+						</Button>
+					</div>
+				</div>
+
+				<div className={` ${styles.grid} ${numOfDivs.length === 12 ? styles.easy : ""} ${numOfDivs.length === 16 ? styles.medium : ""} ${numOfDivs.length === 20 ? styles.hard : ""}`}>
+					{numOfDivs.map(el => (
+						<div key={el}></div>
+					))}
+				</div>
+			</div>
+		</Container>
+	);
 };
