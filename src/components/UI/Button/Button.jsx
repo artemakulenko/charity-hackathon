436c65529fbd1378f5fac7457bd99873b1e170a1
irenekurbanova <kirina2504@gmail.com>
2023-11-12 15:31:42 +0300

    btn styles

--- src/components/UI/Button/Button.jsx
+++ src/components/UI/Button/Button.jsx
@@ -1,10 +1,15 @@
 import PropTypes from "prop-types";
+import styles from "./Button.module.css";
 
-export const Button = ({ onClick, children }) => {
-	return <button onClick={onClick}> {children} </button>;
+export const Button = ({ onClick, children, style, value }) => {
+  return (
+    <button className={styles[style]} onClick={onClick} value={value}>
+      {children}
+    </button>
+  );
 };
 
 Button.propTypes = {
-	onClick: PropTypes.func,
-	children: PropTypes.any,
+  onClick: PropTypes.func,
+  children: PropTypes.any,
 };
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,69 +1,75 @@
 import Container from "../UI/Container/Container";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { Button } from "../UI/Button/Button";
 import styles from "./Home.module.css";
 
 export const Home = () => {
-	const navigate = useNavigate();
-	const [numOfDivs, setNumOfDivs] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+  const navigate = useNavigate();
+  const [numOfDivs, setNumOfDivs] = useState([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+  ]);
 
-	const numDivsHandler = event => {
-		const array = [];
-		let quantity = event.target.value;
-		for (let i = 0; i < quantity; i++) {
-			array.push(i);
-		}
+  const numDivsHandler = (event) => {
+    const array = [];
+    let quantity = event.currentTarget.value;
+    for (let i = 0; i < quantity; i++) {
+      array.push(i);
+    }
 
-		setNumOfDivs(array);
-	};
+    setNumOfDivs(array);
+  };
 
-	const sendSubmit = link => {
-		navigate(link);
-	};
+  const sendSubmit = (link) => {
+    navigate(link);
+  };
 
-	return (
-		<Container>
-			<h1>Выберете сложность</h1>
-			<div className={styles.wrapper}>
-				<div className={styles.actions}>
-					<button
-						value={12}
-						// onClick={() => sendSubmit(/game/easy)}
-						onClick={event => numDivsHandler(event)}
-					>
-						Easy
-					</button>
-					<button
-						value={16}
-						// onClick={() => sendSubmit(/game/medium)}
-						onClick={event => numDivsHandler(event)}
-					>
-						Medium
-					</button>
-					<button
-						value={20}
-						// onClick={() => sendSubmit(/game/hard)}
-						onClick={event => numDivsHandler(event)}
-					>
-						Hard
-					</button>
-					<button type="button" onClick={() => sendSubmit(`/game/${numOfDivs.length}`)}>
-						Играть
-					</button>
-				</div>
+  return (
+    <Container>
+      <h1>Выберете сложность</h1>
+      <div className={styles.wrapper}>
+        <div className={styles.actions}>
+          <button
+            value={12}
+            // onClick={() => sendSubmit(/game/easy)}
+            onClick={(event) => numDivsHandler(event)}
+          >
+            Easy
+          </button>
+          <button
+            value={16}
+            // onClick={() => sendSubmit(/game/medium)}
+            onClick={(event) => numDivsHandler(event)}
+          >
+            Medium
+          </button>
+          <button
+            value={20}
+            // onClick={() => sendSubmit(/game/hard)}
+            onClick={(event) => numDivsHandler(event)}
+          >
+            Hard
+          </button>
+          <button
+            type="button"
+            onClick={() => sendSubmit(`/game/${numOfDivs.length}`)}
+          >
+            Играть
+          </button>
+        </div>
 
-				<div
-					className={`
+        <div
+          className={`
             ${styles.grid} 
             ${numOfDivs.length === 12 && styles.easy} 
             ${numOfDivs.length === 16 && styles.medium} 
             ${numOfDivs.length === 20 && styles.hard}`}
-				>
-					{numOfDivs.map(el => (
-						<div key={el}></div>
-					))}
-				</div>
-			</div>
-		</Container>
-	);
+        >
+          {numOfDivs.map((el) => (
+            <div key={el}></div>
+          ))}
+        </div>
+      </div>
+    </Container>
+  );
 };
